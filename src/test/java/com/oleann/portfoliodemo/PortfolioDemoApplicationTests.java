21e091d29d507f3d4d60303c1053e06ed6a0dbc4
Anna <anna-olejnik10@wp.pl>
2020-11-21 11:45:21 +0100

    structure :: base structure

---
+++ src/test/java/com/oleann/portfoliodemo/PortfolioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleann.portfoliodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
